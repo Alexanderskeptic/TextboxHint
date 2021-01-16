4d6ae465b4722037d67683844b5669724bfe627b
Alexander Pimenov <alexanderskeptic@outlook.com>
2021-01-16 17:48:47 +0300

    Добавлены

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace TextboxHint
         {
             InitializeComponent();
         }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
     }
 }
